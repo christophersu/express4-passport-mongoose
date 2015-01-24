74bf79a78734ced9fc6e84eb4d05ff383066c6b0
Christopher Su <christophersu9@gmail.com>
2015-01-24 12:42:02 -0800

    auth route

--- app.js
+++ app.js
@@ -7,25 +7,10 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var auth = require('./routes/auth');
 
 var app = express();
 
-var secrets = require('./config/secrets');
-var passport = require('passport')
-  , FacebookStrategy = require('passport-facebook').Strategy;
-
-passport.use(new FacebookStrategy({
-    clientID: secrets.Secrets.FACEBOOK.CLIENT_ID,
-    clientSecret: secrets.Secrets.FACEBOOK.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate(..., function(err, user) {
-      if (err) { return done(err); }
-      done(null, user);
-    });
-  }
-));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+var secrets = require('./config/secrets');
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: secrets.Secrets.FACEBOOK.CLIENT_ID,
+    clientSecret: secrets.Secrets.FACEBOOK.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(..., function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+router.get('/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+router.get('/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
+
+module.exports = router;
